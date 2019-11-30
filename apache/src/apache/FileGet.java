93b94bee6cd3e309479850bf67b0a9bab142f452
dada9901 <32613355+dada9901@users.noreply.github.com>
2019-11-30 13:13:10 +0800

    Add files via upload

---
+++ apache/src/apache/FileGet.java
@@ -0,0 +1,41 @@
+package apache;
+import java.io.*;
+
+/**
+ * 从文件中读取全部内容到StringBuffer中
+ */
+public class FileGet{
+    StringBuffer s;
+    public boolean check(String s) {
+        boolean b = false;
+ 
+        String tmp = s;
+        tmp = tmp.replaceAll("\\p{P}", "");
+        if (s.length() != tmp.length()) {
+            b = true;
+        }
+ 
+        return b;
+    }
+    public FileGet(String fileName){
+    	s=new StringBuffer("");
+        try {
+	        FileReader fr = new FileReader(fileName);
+	    int ch;
+	    while((ch=fr.read())!=-1)
+	    {
+	    	
+	    	s.append((char)ch);
+	    }
+	    fr.close();
+		}
+		catch(IOException e)
+		{
+			System.out.println(e.toString());
+		}
+    }
+
+    public String toString(){
+        return s.toString();
+    }
+}
